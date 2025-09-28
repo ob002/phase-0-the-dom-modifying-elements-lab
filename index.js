33a2e037c39dab2d349ab1882273fa7616686d3e
Amos Ombworo <amosombworo@gmail.com>
2025-09-28 22:49:14 +0300

    initial

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code here!// Write your code here!
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.append(newHeader);
